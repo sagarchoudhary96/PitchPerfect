2759dd201384319195b112eeadae0d4e00d753bd
Sagar Choudhary <sagar.choudhary96@gmail.com>
2018-10-09 17:22:32 +0530

    Add: image assets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Tap to record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
 }
-
